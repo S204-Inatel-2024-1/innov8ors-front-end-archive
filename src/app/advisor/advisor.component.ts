6893999049e413f8a1462be9252714348b963f91
Lucas Couto <lucascouto04@hotmail.com>
2024-03-23 19:12:00 -0300

    Expansion attempt

--- src/app/advisor/advisor.component.ts
+++ src/app/advisor/advisor.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import {MatExpansionModule} from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-advisor',
   standalone: true,
-  imports: [MatExpansionModule],
+  imports: [MatExpansionModule, MatFormFieldModule],
   templateUrl: './advisor.component.html',
   styleUrl: './advisor.component.css'
 })
 export class AdvisorComponent {
-
+  panelOpenState: boolean = false;
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatAccordion, MatExpansionPanel } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatAccordion, MatExpansionPanel],
+  imports: [CommonModule, RouterModule, MatAccordion, MatExpansionPanel, MatInputModule, MatFormFieldModule, MatExpansionModule],
   templateUrl: './index.html',
   styleUrl: './app.component.css'
 })
